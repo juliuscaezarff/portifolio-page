48e29d29ddb6c3fd718c9d2b8862fbe758157025
Julius Caezar <julius.caezar25@gmail.com>
2023-06-13 15:59:14 -0300

    creating contact section

---
+++ src/app/terminal/contact/page.tsx
@@ -0,0 +1,27 @@
+import { CodePreview } from '@/components/CodePreview'
+import shiki from 'shiki'
+
+export const revalidate = 1800 // revalidate every 30 minutes
+export const metadata = {
+  title: 'Fish',
+}
+
+export default async function Contact() {
+
+  const code = `.contact {
+    email: 'julius.caezar25@gmail.com';
+    linkedin: 'www.linkedin.com/in/julius-caezar';
+  }`
+
+  const highlighter = await shiki.getHighlighter({
+    theme: 'rose-pine-moon'
+  })
+
+  const output = highlighter.codeToHtml(code, { lang: 'css' })
+
+  return (
+    <>
+      <CodePreview code={output} />
+    </>
+  )
+}

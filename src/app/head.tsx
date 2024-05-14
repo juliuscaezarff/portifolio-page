59dd763b07ada71eb9eb65fee690bfd748073dc6
julius caezar <julius.caezar25@gmail.com>
2024-05-14 19:05:11 -0400

    feat: new favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
     </>
   )
 }
